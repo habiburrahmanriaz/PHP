04d5f2ff74da37ee7ef01150e445cc6855160328
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-12-14 00:29:59 +0600

    var & data types

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-eqho "Hello World";
+// PHP Var
+// PHP Data types
+// PHP Error
+
+$userName = 'Jhon Deo';
+$number = 1; // enteger
+$number2 = 1.5; // float
+$isEnrolled = true; // Boolean
+$students = array( 'Habib', 'Abdullah', 'Jubu' );
+
+$nullValue = null;
+
+var_dump( $isEnrolled );
+
+
+//echo $username;
 ?>
