10082e4889acb7c3accbf9cb0e7a952c6ca5129e
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-12-16 00:56:45 +0600

    Associative Array

--- 02-Array-and-super-globals/index.php
+++ 02-Array-and-super-globals/index.php
@@ -1,1 +1,26 @@
 <?php
+// Array 
+// Associative
+// Super global variables
+
+// $students = array('Jubayer', 'Abdullah', 'Habib');
+
+$students = array(
+  '1st' => array('Jubayer', 'Abdullah'),
+  '2st' =>array('Habib', 'Nasir'),
+  '3st' =>array('Rohim', 'karim')
+);
+
+// $number = array(
+// 1, 2, 3, 4
+//);
+
+$number = array(
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+);
+
+value:
+echo $number['three'];
+
---
+++ 02-Array-and-super-globals/server-info.php
@@ -0,0 +1,8 @@
+<?php
+
+$server_info = $_SERVER;
+echo $server_info['REMOTE_ADDR'];
+
+
+echo "<pre>";
+print_r( $_SERVER);
