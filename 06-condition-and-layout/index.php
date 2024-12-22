26803d1b1f6e571abe5e5842da68bd349356fd6b
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-12-23 00:53:51 +0600

    Condition, switch, header, include

---
+++ 06-condition-and-layout/index.php
@@ -0,0 +1,35 @@
+<?php
+$userAge = 40;
+
+// if ($userAge == 20){
+//   echo "User age is 20";
+// }elseif($userAge == 30){
+//   echo "User age is 30";
+// }elseif($userAge == 40){
+//   echo "User age is 40";
+// }else{
+//   echo "User age is undefined";
+// }
+
+// multiple value 
+// if($userAge > 20 || $userAge < 30){
+//   echo "You are accepted";
+// }
+
+//$isLoggedin = true;
+
+// if($isLoggedin){
+//   echo "Yor are logged in.";
+//   echo "<button> Logout </button>";
+// }else{
+//   echo " You are logged out.";
+//   echo "<button> Login </button";
+// }
+
+/*
+include("layout/footer.php");
+include("layout/header.php");
+/*
+  
+
+
