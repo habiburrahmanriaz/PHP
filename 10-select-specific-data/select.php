98222153bfde819d57802436a794197dd0326549
habiburrahmanriaz <habiburriaz2@gmail.com>
2025-01-30 23:59:09 +0600

    Profile

--- 10-select-specific-data/select.php
+++ 10-select-specific-data/select.php
@@ -3,21 +3,22 @@
 // Database info
 $host = 'localhost';
 $db_user = 'root';
-$db_password = '';
+$db_pass = '';
 $db_name = 'crud';
 
-// Database conn
-$conn = mysqli_connect($host, $db_user, $db_password, $db_name);
+// DB connection
+$conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
 
-//Query
-$user_id = $_REQUEST['id'];
+// Query 
 
-$query = "SELECT * FROM users WHERE id = 'user_id' ";
+$user_id =  $_REQUEST['id'];
 
-// Run the query
+$query = "SELECT * FROM users WHERE id = '$user_id' ";
+
+// Run the query 
 $result = mysqli_query($conn, $query);
 
 // Result
-while( $item = mysqli_fetch_assoc($result)){
-  echo $item['name'];
+while( $item = mysqli_fetch_assoc($result) ){
+	echo $item['name'];
 }
